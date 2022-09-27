7473c607550699d32f24dabe1e7b769579e5472d
polymorphizm <113363819+polymorphizm@users.noreply.github.com>
2022-09-27 12:51:25 +0530

    new commit - fixed indentation

--- src/main/java/com/fnp/Collections/sort-using-comparator/employeestorage/IdComparator.java
+++ src/main/java/com/fnp/Collections/sort-using-comparator/employeestorage/IdComparator.java
@@ -2,9 +2,11 @@ package employeestorage;
 
 import java.util.*;
 
-public class IdComparator implements Comparator<Employee>{
-	@Override
-	public int compare(Employee a, Employee b){
-		return a.id - b.id; 
-	}
+public class IdComparator implements Comparator<Employee>
+
+{
+		@Override
+		public int compare(Employee a, Employee b){
+			return a.id - b.id; 
+		}
 }
