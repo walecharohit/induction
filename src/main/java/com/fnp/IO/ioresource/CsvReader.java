47edefd753e266321d903a6cb515f1be4fb0763d
polymorphizm <113363819+polymorphizm@users.noreply.github.com>
2022-10-03 11:56:23 +0530

    initial commit - after update

--- src/main/java/com/fnp/IO/ioresource/CsvReader.java
+++ src/main/java/com/fnp/IO/ioresource/CsvReader.java
@@ -6,8 +6,8 @@ public class CsvReader implements DataReader{
 	String line;
 	public void getInfo(BufferedReader b)throws IOException{
 		while((line = b.readLine()) != null){
-		String[] arr = line.split(",");
-		System.out.println("ID - "+ arr[0] +" "+" FirstName - "+ arr[1] +" "+" LastName - "+ arr[2] +" "+" Email - "+ arr[3] +" "+" Country - "+ arr[4]);
+			String[] arr = line.split(",");
+			System.out.println("ID - "+ arr[0] +" "+" FirstName - "+ arr[1] +" "+" LastName - "+ arr[2] +" "+" Email - "+ arr[3] +" "+" Country - "+ arr[4]);
 		}
 	}
 }
